955da0163c0bd8e3d6d18a451c5489f260d35f80
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-24 20:24:16 +0530

    commit

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,24 +1,27 @@
 import User from "../models/user.model.js";
-import bcrypt from 'bcryptjs';
-
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generate.token.js";
 
 // REGISTER
 export const userRegister = async (req, res, next) => {
   try {
     const { email, password, ...others } = req.body;
-    const hashPassword = await bcrypt.hash(password,10);
+    const hashPassword = await bcrypt.hash(password, 10);
     const newUser = new User({
       email,
-      password:hashPassword,
+      password: hashPassword,
       ...others,
     });
-    
+    const isUser = await User.findOne({email});
+    if(isUser){
+      res.status(409).json({message:"User already existed!"})
+    }
+    generateToken(res, newUser._id);
     await newUser.save();
     res.status(200).json({ message: "User created", newUser });
   } catch (error) {
     next(error);
   }
-  // console.log(req.body);
 };
 
 // LOGIN
