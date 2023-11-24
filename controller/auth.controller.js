480651dd8adcde87b61d275e1007be8feee6749e
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-24 15:17:16 +0530

    ...

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,9 +1,23 @@
-export const register =()=>{
 
-}
-export const login =()=>{
 
-}
-export const logout =()=>{
-
-}
+export const register = async () => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json({message:"Something went wrong!"})
+    }
+};
+export const login = async () => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json({message:"Something went wrong!"})
+    }
+};
+export const logout = async () => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json({message:"Something went wrong!"})
+    }
+};
