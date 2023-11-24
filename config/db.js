ebe47a0315a15f9757665cfa16f6714ef0f7a9db
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-24 14:23:33 +0530

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
-export const connectDB = async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URL,{
-            autoIndex:true
-        })
-        console.log('Connected')
-        const connect = mongoose.connection;
-        connect.on('connected',()=>{
-            console.log(`MongoDB connected`);
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      autoIndex: true,
+    });
+    console.log("Connected DB");
+    const connect = mongoose.connection;
+    connect.on("connected", () => {
+      console.log(`MongoDB connected`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
