158aa8c2b555ab3423438c50c36547a048861b22
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-24 13:19:34 +0530

    ...

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import cors from 'cors';
+import morgan from "morgan";
+import helmet from "helmet";
+
 
 dotenv.config();
+const app = express();
+
 
 connectDB()
 
-const app = express();
+
+app.use(helmet());
+app.use(morgan('combined'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// Routes ->
+
+
+
+// Error Handling ->
+
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
