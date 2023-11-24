68d82cacd7c02641ad7b10039e715cc2c31188c7
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-24 20:53:31 +0530

    commit

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -21,27 +21,36 @@ export const userRegister = async (req, res, next) => {
     res.status(200).json({ message: "User created", newUser });
   } catch (error) {
     if (err.code === 11000) {
-      res
-        .status(400)
-        .json({
-          message: "Duplicate key error. User with this email already exists",
-        });
+      res.status(400).json({
+        message: "Duplicate key error. User with this email already exists",
+      });
     }
     next(error);
   }
 };
 
 // LOGIN
-export const login = async () => {
+export const login = async (req, res, next) => {
   try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      generateToken(res, user._id);
+      res.status(200).json(user);
+    }
   } catch (error) {
-    res.status(500).json({ message: "Something went wrong!" });
+    next(error);
   }
 };
 
 // LOGOUT
-export const logout = async () => {
+export const logout = async (req,res,next) => {
   try {
+    res.cookie("jwt", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+    res.status(200).json({ message: "User logged out" });
   } catch (error) {
     res.status(500).json({ message: "Something went wrong!" });
   }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,7 +5,7 @@ import { login, logout, userRegister } from "../controller/auth.controller.js";
 const router = express.Router();
 
 router.post("/register", userRegister);
-// router.post("/login", login);
-// router.post("/logout", logout);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
